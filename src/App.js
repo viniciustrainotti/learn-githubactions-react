96ebff52f2ef334349a00f86c45e6be2241779cd
Vinícius Trainotti <vtrainotti@live.com>
2022-09-11 22:37:39 -0300

    feat change phrase

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload again.
+          Edit <code>src/App.js</code> and save to reload test again.
         </p>
         <a
           className="App-link"
