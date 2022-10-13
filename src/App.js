f97998166a757bc7f7cab55f9d21e5dde0b21fb5
Vinícus Trainotti <vinicius.trainotti@objective.com.br>
2022-10-13 16:26:11 -0300

    feat: add new text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload test again guilda.
+          Edit <code>src/App.js</code> and save to reload test again a team.
         </p>
         <a
           className="App-link"
